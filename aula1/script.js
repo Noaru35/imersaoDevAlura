c5ec129ef3ff6da3590042c12a145abce5733e37
Noaru35 <vinicius.s.bastos55@gmail.com>
2025-04-01 18:08:45 -0300

    Primeiro commit no repositório

---
+++ aula1/script.js
@@ -0,0 +1,10 @@
+function conversor(){
+
+    let valor = prompt('Digite um valor em wons');
+    alert(valor);
+    
+    let umWom = 0.0040;
+    
+    let convertido = valor * umWom;
+    alert('R$' + convertido);
+    }
