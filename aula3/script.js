627411028f04d116bdd7dcbd9e90022c0b26fc90
Noaru35 <vinicius.s.bastos55@gmail.com>
2025-04-05 22:59:19 -0300

    Adicionando o conteúdo da aula 3

---
+++ aula3/script.js
@@ -0,0 +1,31 @@
+function jogar() {
+    let rodada = 1;
+    let quebrado = false;
+  
+    while (rodada <= 3  && quebrado == false) {
+      console.log("Rodada: " + rodada);
+  
+      let escolhaJogador = prompt("Nivel " + rodada + ", escolha o vidro (1, 2 ou 3)?");
+      let pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+
+      if (escolhaJogador < 1 || escolhaJogador > 3){
+        alert('Input inválido! Trapaceiros são punidos! O vidro quebrou');
+        quebrado = true;
+      } 
+      
+      if (escolhaJogador == pisoQuebrado) {
+        alert("O vidro quebrou, fim de jogo");
+        quebrado = true;
+      } else {
+        alert("O vidro não quebrou! piso quebrado era o: " + pisoQuebrado);
+      }
+  
+      rodada = rodada + 1;
+  
+      if (rodada == 4) {
+        alert("Você venceu! Vamos jogar novamente");
+        rodada = 0;
+      }
+    }
+  }
+  
